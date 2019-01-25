919e2012ae270649196cc2d2ac8d0e0f77295c2c
Michael Bitokhov <michael.bitokhov@gmail.com>
2019-01-24 23:11:10 -0500

    Add Paths class

--- src/services/Paths.ts
+++ src/services/Paths.ts
@@ -13,23 +13,23 @@ interface PathLocations {
 export class Paths {
   constructor(protected readonly base: PathLocations) {}
 
-  public cwd(path: string = ''): string {
-    return join(this.base.cwd, path);
+  public cwd(...paths: string[]): string {
+    return join(this.base.cwd, ...paths);
   }
 
-  public build(path: string = ''): string {
-    return join(this.base.cwd, path);
+  public build(...paths: string[]): string {
+    return join(this.base.build, ...paths);
   }
 
-  public docs(path: string = ''): string {
-    return join(this.base.cwd, path);
+  public docs(...paths: string[]): string {
+    return join(this.base.docs, ...paths);
   }
 
-  public views(path: string = ''): string {
-    return join(this.base.cwd, path);
+  public views(...paths: string[]): string {
+    return join(this.base.views, ...paths);
   }
 
-  public styles(path: string = ''): string {
-    return join(this.base.cwd, path);
+  public styles(...paths: string[]): string {
+    return join(this.base.styles, ...paths);
   }
 }
