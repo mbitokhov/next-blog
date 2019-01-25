cd31ca376b7af907e7a3808fd06906c7303990ab
Michael Bitokhov <michael.bitokhov@gmail.com>
2019-01-24 23:06:26 -0500

    Remove mongodb and add LevelDB
    Ha. got away without using something as big as Mongo

---
+++ src/container/level.ts
@@ -0,0 +1,14 @@
+import { Container } from 'inversify';
+import leveldown from 'leveldown';
+import levelup from 'levelup';
+import { Paths } from '../services/Paths';
+
+export const LEVEL = Symbol.for('LEVEL_DB');
+
+export function bindLevel(container: Container) {
+  const basePath = container.get(Paths).build('level');
+  const db = levelup(leveldown(basePath));
+
+  container.bind(levelup).toConstantValue(db);
+  container.bind(LEVEL).toConstantValue(db);
+}
